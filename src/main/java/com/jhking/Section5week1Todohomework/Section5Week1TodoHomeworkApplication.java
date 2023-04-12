f1b3bff63510ef6f148d7bb637ff0eb16938a02b
NOTE111 <lolly5488@gmail.com>
2023-04-12 17:25:28 +0900

    initial1

---
+++ src/main/java/com/jhking/Section5week1Todohomework/Section5Week1TodoHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.jhking.Section5week1Todohomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section5Week1TodoHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section5Week1TodoHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhking/Section5week1Todohomework/Section5Week1TodoHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhking.Section5week1Todohomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section5Week1TodoHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
